185832cb318128828961ac3a8b95aee197384d25
Kohinkarsagar <114565952+Kohinkarsagar@users.noreply.github.com>
2024-02-15 13:45:25 +0530

    Add files via upload

---
+++ Spring project/Spring_bootCrud-Day2/src/main/java/com/sbcrud/SpringBootCrudDay2Application.java
@@ -0,0 +1,13 @@
+package com.sbcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudDay2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudDay2Application.class, args);
+	}
+
+}
---
+++ Spring project/Spring_bootCrud-Day2/src/main/java/com/sbcrud/servicei/HomeServiceI.java
@@ -0,0 +1,15 @@
+package com.sbcrud.servicei;
+
+
+
+	import com.sbcrud.model.Student;
+
+	public interface HomeServiceI {
+
+		public void saveData(Student s);
+		public Iterable<Student> displayAll();
+		public Student editData(int uid);
+		public void deleteData(Student s);
+		
+		public Student loginCheck(String un, String ps);
+	}
